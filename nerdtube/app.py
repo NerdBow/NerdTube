5171e941c6d91629b99a0d8b13b7ce48f061f8f3
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-01-29 23:06:46 -0500

    Change positioning of widgets

--- nerdtube/app.py
+++ nerdtube/app.py
@@ -1,15 +1,26 @@
 import customtkinter
 from search_frame import SearchFrame
 
+
 class App(customtkinter.CTk):
     NAME = "NerdTube"
 
+    WIDTH = 800
+    HEIGHT = 800
+
     def __init__(self):
         super().__init__()
+
         self.title(self.NAME)
-        self.geometry("800x800")
+        self.geometry(f"{self.HEIGHT}x{self.WIDTH}")
+        self.minsize(self.WIDTH, self.HEIGHT)
+        self.columnconfigure((0, 1, 2, 3), weight=1)
+        self.rowconfigure((0), weight=1)
+
         self.search_frame = SearchFrame(self)
-        self.search_frame.grid(row=0, column=0, padx=20, pady=20, rowspan=10, columnspan=10)
+        self.search_frame.grid(
+            row=0, column=0, padx=20, pady=20, columnspan=2, sticky="nwe"
+        )
         pass
 
 
