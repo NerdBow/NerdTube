87afc99ad687f9d485d2979eec40919e12e84a22
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-02-18 20:01:19 -0500

    Add class for video objects

---
+++ nerdtube/video_frame.py
@@ -0,0 +1,9 @@
+import customtkinter
+from pytube import Youtube
+from urllib.request import urlopen
+
+class VideoFrame(customtkinter.CTkFrame):
+    """
+    """
+    def __init__(self) -> None:
+        pass
