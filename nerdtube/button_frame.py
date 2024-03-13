fbe736f00018e9c4b7e9327668d6ca1fcd86f044
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-03-13 17:14:44 -0400

    Format buttons on button_frame

--- nerdtube/button_frame.py
+++ nerdtube/button_frame.py
@@ -5,11 +5,14 @@ class ButtonFrame(ctk.CTkFrame):
     def __init__(self, master: ctk.CTkFrame) -> None:
         super().__init__(master=master)
 
-        self.path_button = ctk.CTkButton()
-        self.path_button.grid()
+        self.grid_columnconfigure((0), weight=1)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        self.mp3_switch = ctk.CTkSwitch()
-        self.mp3_switch.grid()
+        self.path_button = ctk.CTkButton(master=self, text="Output Path")
+        self.path_button.grid(row=0, column=0, padx=20, pady=20)
 
-        self.download_button = ctk.CTkButton()
-        self.download_button.grid()
+        self.mp3_switch = ctk.CTkSwitch(master=self, text="MP3")
+        self.mp3_switch.grid(row=1, column=0, padx=20, pady=20)
+
+        self.download_button = ctk.CTkButton(master=self, text="Download")
+        self.download_button.grid(row=2, column=0, padx=20, pady=20)
