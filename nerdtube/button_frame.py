0c09be3479a7f6946d9d6060c9e6b53b53be6f7c
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-03-13 00:19:20 -0400

    Add base components for button_frame

--- nerdtube/button_frame.py
+++ nerdtube/button_frame.py
@@ -1,8 +1,15 @@
 import customtkinter as ctk
 
+
 class ButtonFrame(ctk.CTkFrame):
     def __init__(self, master: ctk.CTkFrame) -> None:
         super().__init__(master=master)
-        
 
-        
+        self.path_button = ctk.CTkButton()
+        self.path_button.grid()
+
+        self.mp3_switch = ctk.CTkSwitch()
+        self.mp3_switch.grid()
+
+        self.download_button = ctk.CTkButton()
+        self.download_button.grid()
